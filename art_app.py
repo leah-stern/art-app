0f7d6dd73633517f0a0c42b5b1b35ecc4c3fb913
Leah Stern <leah.stern@tufts.edu>
2019-03-02 13:13:43 -0500

    fixed Flask bug

--- art_app.py
+++ art_app.py
@@ -8,5 +8,6 @@ def index():
 	return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
-	# app.run(debug=True)
+	app.run()
+# 	app.run(host='0.0.0.0')
+# 	# app.run(debug=True)
