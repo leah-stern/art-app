526dbaffab412a6db32e5613012e99912b8e23e9
Leah Stern <leah.stern@tufts.edu>
2019-03-02 13:29:49 -0500

    fixed Flask bug

--- app/art_app.py
+++ app/art_app.py
@@ -5,8 +5,9 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+        return render_template('index.html')
 
 if __name__ == '__main__':
-	# app.run(host='0.0.0.0')
-	app.run(debug=True)
+        app.run()
+#       app.run(host='0.0.0.0')                                                 
+#       # app.run(debug=True)
